9997313e58bba562051733caa68ed6bf0b441c73
Louay997 <louay.dridi.2005@gmail.com>
2025-02-02 09:04:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
  const ShareButton=document.getElementById('share_button');
  const LinksToShare=document.querySelector('.share_links');
-
+ let n=0;
  ShareButton.addEventListener('click',()=>{
-    ShareButton.style.backgroundColor="hsl(217, 19%, 35%)";
-    ShareButton.style.color="hsl(210, 46%, 95%)";
-    LinksToShare.style.opacity="1";
+    if(n%2==0){
+        ShareButton.style.backgroundColor="hsl(217, 19%, 35%)";
+        ShareButton.style.color="hsl(210, 46%, 95%)";
+        LinksToShare.style.opacity="1";
+     
+    }
+    else{
+        ShareButton.style.backgroundColor="hsl(210, 46%, 95%)";
+        ShareButton.style.color="hsl(217, 19%, 35%)";
+        LinksToShare.style.opacity="0";
+    }
+    n+=1;
+    
 
 
  })
